845ad52955186640a674927b3b034610346bae8a
Alexander Ljungberg <aljungberg@slevenbits.com>
2018-02-15 19:17:08 +0000

    New: ability to apply the right tags to tasks.

---
+++ task_tagger.py
@@ -0,0 +1,92 @@
+import os
+
+from phabricator import Phabricator
+
+
+class TagMap:
+    tag_map = None
+
+    def __init__(self, tag_map):
+        self.tag_map = tag_map
+
+    def tag_phid(self, name):
+        return self.tag_map[name]['phid']
+
+    def tag_name(self, phid):
+        return next((tag['fields']['name'] for tag in self.tag_map.values() if tag['phid'] == phid), None)
+
+    def __getattr__(self, item):
+        return self.tag_map.__getattribute__(item)
+
+    def __getitem__(self, item):
+        return self.tag_phid(item)
+
+
+def resolve_tags(phab):
+    return TagMap({
+        'has_accepted_diff_tag': phab.project.search(constraints={"name": "Accepted"})['data'][0],
+        'has_revision_required_diff_tag': phab.project.search(constraints={"name": "Reviewed"})['data'][0],
+        'has_diff_tag': phab.project.search(constraints={"name": "Diff"})['data'][0],
+    })
+
+
+def on_task_update(phab, task_id, tag_map):
+    task = phab.maniphest.search(constraints={"ids": [task_id]}, attachments={"columns": True, "projects": True})['data'][0]
+    diff_phids = [edge['destinationPHID'] for edge in phab.edge.search(sourcePHIDs=[task['phid']], types=["task.revision"])['data'] if edge['edgeType'] == "task.revision"]
+
+    accepted_count = 0
+    needs_revision_count = 0
+    diffs = phab.differential.revision.search(constraints={"phids": diff_phids})['data']
+
+    for diff in diffs:
+        if diff['fields']['status']['value'] == 'accepted':
+            accepted_count += 1
+        elif diff['fields']['status']['value'] == 'needs-revision':
+            needs_revision_count += 1
+
+    all_tags = {project for project in task['attachments']['projects']['projectPHIDs']}
+    my_tags = all_tags.intersection({tag['phid'] for tag in tag_map.values()})
+    new_tags = set()
+
+    if diffs:
+        if accepted_count:
+            new_tags.add(tag_map['has_accepted_diff_tag'])
+        else:
+            new_tags.discard(tag_map['has_accepted_diff_tag'])
+
+        if needs_revision_count:
+            new_tags.add(tag_map['has_revision_required_diff_tag'])
+        else:
+            new_tags.discard(tag_map['has_revision_required_diff_tag'])
+
+        if not new_tags:
+            # If neither accepted nor reviewed, at least reveal that there is a diff.
+            new_tags.add(tag_map['has_diff_tag'])
+        else:
+            new_tags.discard(tag_map['has_diff_tag'])
+
+    transactions = []
+
+    tags_to_add = new_tags - all_tags
+    if tags_to_add:
+        print("Tagging T{} with {}.".format(task['id'],  ", ".join(tag_map.tag_name(phid) for phid in tags_to_add)))
+        transactions.append({"type": "projects.add", "value": list(tags_to_add)})
+
+    tags_to_remove = my_tags - new_tags
+    if tags_to_remove:
+        print("Untagging T{} with {}.".format(task['id'],  ", ".join(tag_map.tag_name(phid) for phid in tags_to_remove)))
+        transactions.append({"type": "projects.remove", "value": list(tags_to_remove)})
+
+    if transactions:
+        phab.maniphest.edit(objectIdentifier=task['phid'], transactions=transactions)
+
+
+if __name__ == '__main__':
+    PHABRICATOR_API_URL = os.environ['PHABRICATOR_API_URL']
+
+    phab = Phabricator(host=PHABRICATOR_API_URL)
+    phab.update_interfaces()
+
+    tags = resolve_tags(phab)
+
+    on_task_update(phab, 2895, tag_map=tags)
---
+++ task_tagger_test.py
@@ -0,0 +1,78 @@
+import re
+
+import vcr
+from nose.tools import eq_, ok_
+from phabricator import Phabricator
+
+from task_tagger import resolve_tags, on_task_update
+
+
+def before_record_cb(request):
+    # Don't save the real token used when recording the VCR cassette. Tokens are in the format 'cli-...'.
+    request.body = re.sub(r'cli-[\w\d]+', '123', request.body.decode('utf-8')).encode('utf-8')
+    return request
+
+
+def before_record_response(response):
+    if 'Set-Cookie' in response['headers']:
+        del response['headers']['Set-Cookie']
+    return response
+
+
+my_vcr = vcr.VCR(
+    before_record_request = before_record_cb,
+    before_record_response=before_record_response,
+)
+
+@my_vcr.use_cassette('fixtures/vcr_cassettes/resolve_tags.yaml')
+def test_resolve_tags__should_find_standard_tags():
+    phab = Phabricator(host="https://truecode.trueship.com/api/")
+    tag_map = resolve_tags(phab)
+
+    eq_(tag_map['has_revision_required_diff_tag'], "PHID-PROJ-kx4rkibaeu6rt72oupll")
+    eq_(tag_map['has_diff_tag'], "PHID-PROJ-segawqxql2j3qedl7cqc")
+    eq_(tag_map['has_accepted_diff_tag'], "PHID-PROJ-72kxzxxqoak5bp2llx3m")
+
+
+@my_vcr.use_cassette('fixtures/vcr_cassettes/on_update_task_2895.yaml', inject_cassette=True)
+def test_on_task_update__for_task_with_needs_changes_diff__should_tag_with_reviewed_tag(cassette):
+    phab = Phabricator(host="https://truecode.trueship.com/api/")
+    tag_map = resolve_tags(phab)
+
+    on_task_update(phab, 2895, tag_map)
+
+    eq_(cassette.requests[-1].url, "https://truecode.trueship.com/api/maniphest.edit")
+    eq_(cassette.requests[-1].body, b"output=json&params=%7B%22__conduit__%22%3A+%7B%22token%22%3A+%22123%22%7D%2C+%22objectIdentifier%22%3A+%22PHID-TASK-bdaz2w2ue4nlotls6wqn%22%2C+%22transactions%22%3A+%5B%7B%22type%22%3A+%22projects.add%22%2C+%22value%22%3A+%5B%22PHID-PROJ-kx4rkibaeu6rt72oupll%22%5D%7D%5D%7D")
+
+
+@my_vcr.use_cassette('fixtures/vcr_cassettes/on_update_task_3070.yaml', inject_cassette=True)
+def test_on_task_update__for_task_with_accepted_diff__should_tag_with_accepted_tag(cassette):
+    phab = Phabricator(host="https://truecode.trueship.com/api/")
+    tag_map = resolve_tags(phab)
+
+    on_task_update(phab, 3070, tag_map)
+
+    eq_(cassette.requests[-1].url, "https://truecode.trueship.com/api/maniphest.edit")
+    eq_(cassette.requests[-1].body, b"params=%7B%22objectIdentifier%22%3A+%22PHID-TASK-djleagj2aiegvb7kzivn%22%2C+%22__conduit__%22%3A+%7B%22token%22%3A+%22123%22%7D%2C+%22transactions%22%3A+%5B%7B%22value%22%3A+%5B%22PHID-PROJ-72kxzxxqoak5bp2llx3m%22%5D%2C+%22type%22%3A+%22projects.add%22%7D%5D%7D&output=json")
+
+
+@my_vcr.use_cassette('fixtures/vcr_cassettes/on_update_task_3109.yaml', inject_cassette=True)
+def test_on_task_update__for_task_with_unreviewed_diff__should_tag_with_diff_tag(cassette):
+    phab = Phabricator(host="https://truecode.trueship.com/api/")
+    tag_map = resolve_tags(phab)
+
+    on_task_update(phab, 3109, tag_map)
+
+    eq_(cassette.requests[-1].url, "https://truecode.trueship.com/api/maniphest.edit")
+    eq_(cassette.requests[-1].body, b"params=%7B%22transactions%22%3A+%5B%7B%22value%22%3A+%5B%22PHID-PROJ-segawqxql2j3qedl7cqc%22%5D%2C+%22type%22%3A+%22projects.add%22%7D%5D%2C+%22objectIdentifier%22%3A+%22PHID-TASK-s3scdbecgvk23b4uwsfp%22%2C+%22__conduit__%22%3A+%7B%22token%22%3A+%22123%22%7D%7D&output=json")
+
+
+@my_vcr.use_cassette('fixtures/vcr_cassettes/on_update_task_2895_extra_tags.yaml', inject_cassette=True)
+def test_on_task_update__for_task_with_needs_changes_diff__should_remove_undesired_tags(cassette):
+    phab = Phabricator(host="https://truecode.trueship.com/api/")
+    tag_map = resolve_tags(phab)
+
+    on_task_update(phab, 2895, tag_map)
+
+    eq_(cassette.requests[-1].url, "https://truecode.trueship.com/api/maniphest.edit")
+    eq_(cassette.requests[-1].body, b"params=%7B%22objectIdentifier%22%3A+%22PHID-TASK-bdaz2w2ue4nlotls6wqn%22%2C+%22transactions%22%3A+%5B%7B%22value%22%3A+%5B%22PHID-PROJ-kx4rkibaeu6rt72oupll%22%5D%2C+%22type%22%3A+%22projects.add%22%7D%2C+%7B%22value%22%3A+%5B%22PHID-PROJ-segawqxql2j3qedl7cqc%22%2C+%22PHID-PROJ-72kxzxxqoak5bp2llx3m%22%5D%2C+%22type%22%3A+%22projects.remove%22%7D%5D%2C+%22__conduit__%22%3A+%7B%22token%22%3A+%22123%22%7D%7D&output=json")
