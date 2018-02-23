7fa97e0cf6e7d4c828574a4e3ac866d9cf7b2813
Alexander Ljungberg <aljungberg@slevenbits.com>
2018-02-23 11:59:20 +0000

    Fixed: incorrect API URL for tests.

--- bin/ci_test.sh
+++ bin/ci_test.sh
@@ -9,7 +9,6 @@ echo "Running main tests..."
 TEAMCITY_VERSION=${TEAMCITY_VERSION:-0}
 
 DEV_MOUNT=""
-PHABRICATOR_API_URL="https://truecode.trueship.com/api/"
 
 if [[ "${DEV:-0}" == "1" ]]; then
     DEV_MOUNT="-v$SOURCEDIR:/app:rw"
@@ -19,7 +18,7 @@ docker run \
     --rm \
     --name="$APP"-tests \
     $DEV_MOUNT \
-    -e "PHABRICATOR_API_URL=PHABRICATOR_API_URL" \
+    -e "PHABRICATOR_API_URL=https://truecode.trueship.com/api/" \
     -e "PHABRICATOR_API_TOKEN=api-123" \
     -e "TEAMCITY_VERSION=$TEAMCITY_VERSION" \
     "gcr.io/rc-apps-images/$APP-tests:$GITHASH" \
